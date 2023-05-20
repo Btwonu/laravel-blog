90edc9d7617976a53653aa3eb63e150666fbce2f
vdelchev <vdelchev@htmlbox.net>
2023-05-20 18:47:56 +0300

    Add categories select and tags to home page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,29 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
+	<section class="section-form my-8">
+		<div class="shell mx-auto max-w-screen-xl px-4">
+			<div class="section__inner md:flex md:justify-between">
+				<form method="get" action="{{ route('home') }}" class="form w-full justify-center md:flex md:flex-wrap md:justify-between">
+					<div class="form__field flex justify-center md:mt-0 md:min-w-[20rem]">
+						<label class="hidden" for="p-search">Search posts</label>
 
-<section class="section-form my-8">
-	<div class="shell max-w-screen-xl mx-auto px-4">
-			<form action="{{ route('home') }}" class="form flex justify-center">
-				<label class="hidden" for="p-search">Search posts</label>
+						<input type="search" placeholder="Search posts" name="search" id="p-search" value="{{ request('search') }}"
+							class="input-bordered input-info input w-full max-w-xs" />
+					</div><!-- /.form__field -->
 
-				<input 
-					type="search" 
-					placeholder="Search posts" 
-					name="search" 
-					id="p-search" 
-					value="{{ request('search') }}" 
-					class="input input-bordered input-info w-full max-w-xs" 
-				/>
-			</form><!-- /.form -->
+					<div class="form__field mt-4 flex justify-center md:mt-0 md:min-w-[20rem]">
+						<label for="s-category" class="hidden"></label><!-- /.hidden -->
+
+						<select id="s-category" class="select-info select w-full max-w-xs" name="category" value="{{ request('category') }}">
+							<option selected value=''>Category</option>
+
+							@foreach ($categories as $category)
+								<option value="{{ $category->slug }}" {{ request('category') == $category->slug ? 'selected' : '' }}>{{ $category->title }}</option>
+							@endforeach
+						</select>
+					</div><!-- /.form__field -->
+
+					<div class="form__field js-category-checkboxes mt-8 w-full">
+						<div class="tags">
+							<div class="tags__items flex flex-wrap justify-center gap-4 md:justify-start">
+								@foreach ($tags as $tag)
+									<div class="tags__item">
+										<div class="tag relative">
+											<label class="absolute inset-0 cursor-pointer" for="{{ 'tag-' . $tag->id }}"></label>
+
+											<input id="{{ 'tag-' . $tag->id }}" class="peer hidden" type="checkbox" name="tags" />
+
+											<span
+												class="leading-sm inline-flex items-center rounded-full bg-orange-200 py-2 px-4 text-xs font-bold uppercase peer-checked:bg-orange-400">{{ $tag->title }}</span>
+										</div><!-- /.tag -->
+									</div><!-- /.tags__item -->
+								@endforeach
+							</div><!-- /.tags__items -->
+						</div><!-- /.tags -->
+					</div><!-- /.form__field -->
+				</form><!-- /.form -->
+			</div><!-- /.section__inner -->
 		</div><!-- /.shell -->
 	</section><!-- /.section-form -->
-	
+
 	<section class="section-posts my-8">
-		<div class="shell max-w-screen-xl mx-auto px-4">
+		<div class="shell mx-auto max-w-screen-xl px-4">
 			@if ($posts->count())
 				<div class="posts">
-					<div class="posts__items md:flex md:flex-wrap">
+					<div class="posts__items md:-mx-4 md:flex md:flex-wrap">
 						@foreach ($posts as $post)
 							<div class="posts__item flex flex-2 flex-grow py-4 md:p-4 lg:flex-3">
 								<x-post :id="$post->id" :title="$post->title"></x-post>
